87bcfb7f5f95146165b626c87afc08a2bda4693a
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2016-07-04 17:29:52 -0400

    third iteration completed with word counter, estimated reading time, and word frequency counter all working.

--- week_4/word_central/app/controllers/text_inspections_controller.rb
+++ week_4/word_central/app/controllers/text_inspections_controller.rb
@@ -11,7 +11,20 @@ class TextInspectionsController < ApplicationController
 
 		@read_time = @wordcount/275
 
+		@top_words = word_freq(@text)
+
 		render "results"
 	end
+
+	private
+
+	def word_freq(string)
+  		words = string.split(' ')
+  		frequency = Hash.new(0)
+  		words.each { |word| frequency[word.downcase] += 1 }
+		sorted_freq = frequency.sort { |l, r| r[1]<=>l[1] }
+		top_words = sorted_freq[0..9]
+		return top_words
+	end 
 		
 end
