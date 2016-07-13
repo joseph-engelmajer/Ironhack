47b0667fc8c7c1c54dbc54c73665de5deb30fc56
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2016-07-13 19:27:05 -0400

    first iteration done. album search returning proper object from api. working on displaying albums in bootstrap modal

--- week_5/spotify/app.js
+++ week_5/spotify/app.js
@@ -1,39 +1,117 @@
 $(document).ready(function(){
+
 	$('.js-artist-search-form').on("submit", function (event) {
+		
 		event.preventDefault();
-		fetchArtists()
+		fetchArtists() // calling function that submits ajax request
+
+//===================================================================================================
+//================ Ajax Request For Artist Info =====================================================
+//=================================================================================================== 
+		
 		function fetchArtists() {
-			var input = $('.js-artist').val()
-	    	$('.artistList').empty();	
+			
+			var input = $('.js-artist').val() //sets var = to the user input in search form
+	    	
+	    	$('.artistList').empty(); //empties the list of artists before appending new search to avoid duplicate data	
+	    	
 	    	$.ajax({
 	            type: "GET",
 	            url: `https://api.spotify.com/v1/search?type=artist&query=${input}`,
-	            success: function (response) {
-	              console.log("success!")
-	              console.log(response);
-	              // showCharacters(response);
-	            },
-	            error: function (error) {
-	              console.log("Error!");
-	              // console.log(error.responseText);
-	            }
+	            success: showArtists,
+	            error: handleError
 	        });		        		   
 	    }
 	    console.log("form submitted!")
-	});    
+	});  
 
-    function showArtists (responseArray) {
-    	responseArray.forEach(function (theArtist) {
+//===================================================================================================
+//================ Ajax Request For Artist's Albums =================================================
+//=================================================================================================== 
+		
+		function fetchAlbums() {
+			
+		    $('.artistImg').click(function(event){
+		    	var artistId = $(event.currentTarget).data('id')
+		    	$.ajax({
+		            type: "GET",
+		            url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+		            success: function(response){
+		            	console.log(response)
+		            },
+		            error: handleError
+		        });		        		   
+		    })
+		};		 
+
+
+//===================================================================================================
+//==================== Display Artist Info ==========================================================
+//=================================================================================================== 
+
+    function showArtists (response) {
+    	response.artists.items.forEach(function (theArtist) {
             var html = `
 				<li> 
-												
+					<h2>${theArtist.name}</h2> 
+					<img src=${theArtist.images[0].url} data-id=${theArtist.id} class="artistImg">							
 				</li>
             `
-      
+      		
+      		console.log(response);
             $('.artistList').prepend(html);
+            fetchAlbums();
         });
     };
 
+//===================================================================================================
+//==================== Display Artist Albums ========================================================
+//=================================================================================================== 
+
+    function showArtists (response) {
+    	response.artists.items.forEach(function (theArtist) {
+            var html = `
+				<li> 
+					<h2>${theArtist.name}</h2> 
+					<img src=${theArtist.images[0].url} data-id=${theArtist.id} class="artistImg">							
+				</li>
+            `
+      		
+      		console.log(response);
+            $('.artistList').prepend(html);
+            fetchAlbums();
+        });
+    };
+
+//===================================================================================================
+//==================== Display Error Function =======================================================
+//=================================================================================================== 
+
+	function handleError (error) {
+	  console.log("Watch yo Back! We got an urrur");
+	  console.log(error.responseText);
+	}
+//===================================================================================================
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
