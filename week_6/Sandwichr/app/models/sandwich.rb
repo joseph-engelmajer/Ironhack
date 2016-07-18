56e9403d277ac334e49dc54f886d5a664de24091
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2016-07-18 17:42:12 -0400

    added total calories column to sandwich db. succesfully adds ingredient calories for a total

--- week_6/Sandwichr/app/models/sandwich.rb
+++ week_6/Sandwichr/app/models/sandwich.rb
@@ -1,4 +1,9 @@
 class Sandwich < ApplicationRecord
 	has_many :sandwichingredients
-	has_many :ingredients, through: "sandwichingredients"
+	has_many :ingredients, through: :sandwichingredients, after_add: :add_calories
+
+	def add_calories(argument)
+		self.total_calories += argument.calories
+		save		
+	end
 end
---
+++ week_6/Sandwichr/db/migrate/20160718212433_change_column.rb
@@ -0,0 +1,5 @@
+class ChangeColumn < ActiveRecord::Migration[5.0]
+  def change
+  	change_column :sandwiches, :total_calories, :integer, default: 0
+  end
+end
